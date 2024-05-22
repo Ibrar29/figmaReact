c8f506f30a7c557ab73a60d9a1b49d1a337a003e
Ibrar29 <156094384+Ibrar29@users.noreply.github.com>
2024-05-22 01:44:04 -0700

    ready

---
+++ src/pages/Works.jsx
@@ -0,0 +1,320 @@
+import React from "react";
+
+const Works = () => {
+  return (
+    <>
+      <div className="flex justify-center lg:mb-4 ">
+        <hr className="w-10 h-[5px] bg-[#DE4396] mb-4 md:mb-0 md:mr-4" />
+      </div>
+      <div className="flex justify-center items-center ">
+        <div className="text-center">
+          <h1 className=" lg:mb-3 sm-mb-1">Way of building </h1>
+          <h1 className="font-bold sm:mb-40 ">Great Softwares</h1>
+        </div>
+      </div>
+      <div className="hidden md:block lg:text-center">
+        <img src="Ellipse 177.png" alt="ll" className="mx-auto" />
+      </div>
+
+      <div className=" lg:m-24  md:m-6 sm:m-1 translate-y-[-128px] ">
+        {/* card1 */}
+        <div className="lg:flex sm:block ">
+          {/* Text */}
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold mt-8 ">
+              Build the right team to scale
+            </h1>
+            <p className="mb-6 lg:leading-loose text-[#2D3748] sm:leading-tight sm:text-wrap">
+              Finding the right talent is not easy. We help you find the talent
+              <br className="hidden md:inline" />
+              that suits your needs, follows your processes, and sticks with
+              <br className="hidden md:inline" />
+              you long term (not the case with freelancers)
+            </p>
+            <p className="mb-6 leading-loose text-[#2D3748]">
+              Our <span className="text-[#DE4396]">delivery model </span>
+              helps you cut costs and deliver within
+              <br className="hidden md:inline " />
+              budget.
+            </p>
+            <div className="flex">
+              <div className="h-24 border-2 border-[#DE4396]"></div>
+
+              <p className="mb-6 leading-loose  bg-gradient-to-r from-[#DE4396] to-[#0D1C9F] text-transparent bg-clip-text pl-6 ">
+                "Simform is quick to identify larger problem with the
+                <br className="hidden md:inline" />
+                Software so we decided to expand our scope to build
+                <br className="hidden md:inline" />
+                new modules"
+              </p>
+            </div>
+            <div className="flex flex-col  ">
+              <div className="rounded-full overflow-hidden w-10 h-10  flex justify-center items-center">
+                <img
+                  src="p2.jpeg"
+                  alt="2"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <p className="text-sm">Ameen md</p>
+              <p className="text-xs">Ceo</p>
+            </div>
+          </div>
+
+          {/* Image */}
+          <div className=" flex-1 translate-y-[10px]">
+            <img
+              src="Rectangle 17.png"
+              alt="crd"
+              className="lg:w-[575px] lg:h-[473px] sm:w-full sm: h-auto"
+            />
+          </div>
+        </div>
+        <div className=" lg:ml-[950px]">
+          <img src="Ellipse 180.png" alt="ll" className="h-[30px] w-[30px]" />
+        </div>
+
+        {/* second card */}
+        <div className=" lg:ml-[540px]">
+          <img src="Ellipse 180.png" alt="ll" className="h-[30px] w-[30px]" />
+        </div>
+        <div className="lg:flex sm:block ">
+          {/* Image */}
+          <div className=" flex-1 translate-y-[10px]">
+            <img
+              src="Rectangle 18.png"
+              alt="crd"
+              className="lg:w-[575px] lg:h-[473px] sm:w-full sm: h-auto"
+            />
+          </div>
+          {/* Text */}
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold mt-8 ">
+              Build the right team to scale
+            </h1>
+            <p className="mb-6 lg:leading-loose text-[#2D3748] sm:leading-tight sm:text-wrap">
+              Finding the right talent is not easy. We help you find the talent
+              <br className="hidden md:inline" />
+              that suits your needs, follows your processes, and sticks with
+              <br className="hidden md:inline" />
+              you long term (not the case with freelancers)
+            </p>
+            <p className="mb-6 leading-loose text-[#2D3748]">
+              Our <span className="text-[#DE4396]">delivery model </span>
+              helps you cut costs and deliver within
+              <br className="hidden md:inline " />
+              budget.
+            </p>
+            <div className="flex">
+              <div className="h-24 border-2 border-[#DE4396]"></div>
+
+              <p className="mb-6 leading-loose  bg-gradient-to-r from-[#DE4396] to-[#0D1C9F] text-transparent bg-clip-text pl-6 ">
+                "Simform is quick to identify larger problem with the
+                <br className="hidden md:inline" />
+                Software so we decided to expand our scope to build
+                <br className="hidden md:inline" />
+                new modules"
+              </p>
+            </div>
+
+            <div className="flex flex-col  ">
+              <div className="rounded-full overflow-hidden w-10 h-10  flex justify-center items-center">
+                <img
+                  src="p4.jpeg"
+                  alt="2"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <p className="text-sm">Ameen md</p>
+              <p className="text-xs">Ceo</p>
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:block lg:text-center ml-12">
+          <img src="Ellipse 177.png" alt="ll" />
+        </div>
+        {/* third card */}
+        <div className=" lg:ml-[1020px]">
+          <img src="Ellipse 180.png" alt="ll" className="h-[30px] w-[30px]" />
+        </div>
+        <div className="lg:flex sm:block ">
+          {/* Text */}
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold mt-8 ">
+              Build the right team to scale
+            </h1>
+            <p className="mb-6 lg:leading-loose text-[#2D3748] sm:leading-tight sm:text-wrap">
+              Finding the right talent is not easy. We help you find the talent
+              <br className="hidden md:inline" />
+              that suits your needs, follows your processes, and sticks with
+              <br className="hidden md:inline" />
+              you long term (not the case with freelancers)
+            </p>
+            <p className="mb-6 leading-loose text-[#2D3748]">
+              Our <span className="text-[#DE4396]">delivery model </span>
+              helps you cut costs and deliver within
+              <br className="hidden md:inline " />
+              budget.
+            </p>
+            <div className="flex">
+              <div className="h-24 border-2 border-[#DE4396]"></div>
+
+              <p className="mb-6 leading-loose  bg-gradient-to-r from-[#DE4396] to-[#0D1C9F] text-transparent bg-clip-text pl-6 ">
+                "Simform is quick to identify larger problem with the
+                <br className="hidden md:inline" />
+                Software so we decided to expand our scope to build
+                <br className="hidden md:inline" />
+                new modules"
+              </p>
+            </div>
+            <div className="flex flex-col  ">
+              <div className="rounded-full overflow-hidden w-10 h-10  flex justify-center items-center">
+                <img
+                  src="p2.jpeg"
+                  alt="2"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <p className="text-sm">Ameen md</p>
+              <p className="text-xs">Ceo</p>
+            </div>
+          </div>
+
+          {/* Image */}
+          <div className=" flex-1 translate-y-[-9px]">
+            <img
+              src="Rectangle 19 (1).png"
+              alt="crd"
+              className="lg:w-[575px] lg:h-[473px] sm:w-full sm: h-auto"
+            />
+          </div>
+        </div>
+        <div className="hidden md:block lg:text-center ml-[750px] translate-y-[-10px]">
+          <img src="Ellipse 177.png" alt="ll" className="h-[30px] w-[30px]" />
+        </div>
+      </div>
+
+      {/* 2nd section */}
+      <section className="bg-[#F7F7FA] ">
+        <div className="flex justify-center lg:mb-4 ">
+          <hr className="w-10 h-[5px] bg-[#DE4396] mb-4 md:mb-0 md:mr-4" />
+        </div>
+        <div className="lg:flex justify-center items-center ">
+          <div className="text-center">
+            <h1 className=" lg:mb-3 sm-mb-1">Our design and <br />
+             </h1>
+            <h1 className="font-bold ">development approach</h1>
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row justify-center space-y-8 sm:space-y-0 sm:space-x-8 m-20">
+  {/* <!-- First Card --> */}
+  <div className="bg-white p-8 rounded-lg shadow-md">
+    <div className="flex justify-center items-center w-20 h-14 border border-gray-300 rounded-md mt-5 bg-[#061012]">
+      <img src="Rocket_perspective_matte.png" alt="logo" className="w-9 h-9" />
+    </div>
+
+    <div className="mt-5">
+      <h1 className="mb-6">UX Driven Engineering</h1>
+      <p className="text-gray-600">
+        Unlike other companies, we are a UX first development company.
+        <br /> Projects are driven by designers and they make sure design and <br />
+        
+        experiences translate to code.
+      </p>
+    </div>
+  </div>
+
+  {/* <!-- Second Card --> */}
+  <div className="bg-white p-8 rounded-lg shadow-md">
+    <div className="flex justify-center items-center w-20 h-14 border border-gray-300 rounded-md mt-5 bg-[#68DBF2]">
+      <img src="Code_perspective_matte.png" alt="logo" className="w-9 h-9" />
+    </div>
+
+    <div className="mt-5">
+      <h1 className="mb-6">Developing Shared Understanding</h1>
+      <p className="text-gray-600">
+        Unlike other companies, we are a <span className="text-[#68DBF2]"> UX first</span> development company.
+        <br /> Projects are driven by designers and they make sure design and <br />
+        
+        experiences translate to code.
+      </p>
+    </div>
+  </div>
+</div>
+<div className="flex flex-col sm:flex-row justify-center space-y-8 sm:space-y-0 sm:space-x-8 m-20">
+  {/* <!-- First Card --> */}
+  <div className="bg-white p-8 rounded-lg shadow-md">
+    <div className="flex justify-center items-center w-20 h-14 border border-gray-300 rounded-md mt-5 bg-[#FF3D9A]">
+      <img src="Heart_rate_perspective_matte.png" alt="logo" className="w-9 h-9" />
+    </div>
+
+    <div className="mt-5">
+      <h1 className="mb-6">Proven Experience and Expertise</h1>
+      <p className="text-gray-600">
+        Unlike other companies, we are a <span className="text-[#FF3D9A]">UX first</span>  development company.
+        <br /> Projects are driven by designers and they make sure design and <br />
+        
+        experiences translate to code.
+      </p>
+    </div>
+  </div>
+
+  {/* <!-- Second Card --> */}
+  <div className="bg-white p-8 rounded-lg shadow-md">
+    <div className="flex justify-center items-center w-20 h-14 border border-gray-300 rounded-md mt-5 bg-[#24E795]">
+      <img src="Shield_perspective_matte.png" alt="logo" className="w-9 h-9" />
+    </div>
+
+    <div className="mt-5">
+      <h1 className="mb-6">Security & Intellectual Property (IP)</h1>
+      <p className="text-gray-600">
+        Unlike other companies, we are a <span className="text-[#68DBF2]"> UX first</span> development company.
+        <br /> Projects are driven by designers and they make sure design and <br />
+        
+        experiences translate to code.
+      </p>
+    </div>
+  </div>
+</div>
+
+<div className="flex flex-col sm:flex-row justify-center space-y-8 sm:space-y-0 sm:space-x-8 m-20">
+  {/* <!-- First Card --> */}
+  <div className="bg-white p-8 rounded-lg shadow-md">
+    <div className="flex justify-center items-center w-20 h-14 border border-gray-300 rounded-md mt-5 bg-[#FFEF5E]">
+      <img src="Success_perspective_matte.png" alt="logo" className="w-9 h-9" />
+    </div>
+
+    <div className="mt-5">
+      <h1 className="mb-6">Code Reviews</h1>
+      <p className="text-gray-600">
+        Unlike other companies, we are a <span className="text-[#FFEF5E]">UX first</span>  development company.
+        <br /> Projects are driven by designers and they make sure design and <br />
+        
+        experiences translate to code.
+      </p>
+    </div>
+  </div>
+
+  {/* <!-- Second Card --> */}
+  <div className="bg-white p-8 rounded-lg shadow-md">
+    <div className="flex justify-center items-center w-20 h-14 border border-gray-300 rounded-md mt-5 bg-[#F76680]">
+      <img src="Padlock_perspective_matte.png" alt="logo" className="w-9 h-9" />
+    </div>
+
+    <div className="mt-5">
+      <h1 className="mb-6">Quality Assurance & Testing</h1>
+      <p className="text-gray-600">
+        Unlike other companies, we are a <span className="text-[#F76680]"> UX first</span> development company.
+        <br /> Projects are driven by designers and they make sure design and <br />
+        
+        experiences translate to code.
+      </p>
+    </div>
+  </div>
+</div>
+      </section>
+    </>
+  );
+};
+
+export default Works;
